3cb2d52d916a91ec64f4218287b48b5d334407f6
j-west <westja89@gmail.com>
2016-11-29 09:07:37 -0600

    Added regular expression that will be used to search for characters not meant to be in the strings.  Also added a for loop with a nested for loop to find and replace the unwanted chars

--- songs.js
+++ songs.js
@@ -5,3 +5,12 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+var re = [/\*|\@|\!|\(/g, "> "];
+
+for (var i = 0; i < songs.length; i++) {
+  for (var j = 0; j < re.length; j++) {
+    songs[i] = songs[i].replace(re[j], "");
+  }
+  console.log(songs[i]);
+}
