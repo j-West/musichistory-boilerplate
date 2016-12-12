c8aefe527147c63c39d776cdc8491e1813de7734
j-west <westja89@gmail.com>
2016-12-12 14:37:29 -0600

    Add logic to load second JSON file on click of more button.  Added delete button that deletes the song associated with the delete button

--- songs.js
+++ songs.js
@@ -3,98 +3,69 @@ var mainDiv = document.querySelector(".mainDiv")
 var formDiv = document.querySelector("#formDiv")
 var addMusicView = document.querySelector(".add-music-view")
 
-var songs = [];
-// songs.unshift("Broccoli-D.R.A.M.-Big Baby D.R.A.M.-Hip Hop");
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-// songs.push("Black Beatles-Rae Sremmurd-StremmLife2-Hip Hop");
 
-// Regular expression to take out characters that don't belong
-var re = [/\*|\@|\!|\(/g, "> "];
-//Regular expression to get the array formatted
-var reFormat = / \- by | on the album /g;
-// The second argument that will be passed to the replace method calls
-var change = ["", "- "];
+function addUpdatedMusic(library) {
 
-// Outer for loop to loop over each index of songs array
-for (var i = 0; i < songs.length; i++) {
-  // Inner loop to loop over each index of the regEx re array and change array.
-  for (var j = 0; j < re.length; j++) {
-    // Replaces unwanted characters in the current index of songs with wanted characters
-    songs[i] = songs[i].replace(re[j], change[j]);
-  }
-  // console.log(songs[i]);
-  // Replacing all instances of '>' with '-'
-  songs[i] = songs[i].replace(reFormat, "-");
-}
-
-// Push a song and it's info to the end of the songs array and add one to the front with unshift
-songs.push("Black Beatles-Rae Sremmurd-StremmLife2-Hip Hop");
-songs.unshift("Broccoli-D.R.A.M.-Big Baby D.R.A.M.-Hip Hop");
-
-// Use a loop to add genre because it's the same for multiple songs
-for (var i = 1; i < songs.length-2; i++) {
-  songs[i] = songs[i] + "-Rock";
-}
-// Add genre to a single song
-songs[5] = songs[5] + "-Pop";
-
-// console.log(songs);
-
-
-
-
-
-// Object to hold the information about all of the songs in the song array,
-// broken down into song, artist, album, and genre
-var musicLibrary = {
-                    songs : [],
-                    artist : [],
-                    album : [],
-                    genre : []
-                   };
-
-
-// Loop to loop over the songs array
-for (var i = 0; i < songs.length; i++) {
-  // Splitting the current index of songs array by '-' and storing the resulting array in tempArray
-  var tempArray = songs[i].split("-");
-  // console.log(tempArray);
-  // Pushing the indexes of tempArray to the musicLibrary object each time through the loop
-  musicLibrary.songs.push(tempArray[0]);
-  musicLibrary.artist.push(tempArray[1]);
-  musicLibrary.album.push(tempArray[2]);
-  musicLibrary.genre.push(tempArray[3]);
-}
-
-
-
-function addUpdatedMusic() {
-    mainDiv.innerHTML = ""
   // Loop over the musicLibrary object to place each value into the songDiv
-  for (var i = 0; i < musicLibrary.songs.length; i++) {
+  for (let i = 0; i < library.music.length; i++) {
 
 
     let songDiv = document.createElement("div")
     songDiv.classList.add("songDiv")
 
       songDiv.innerHTML = `
-                              <div class="songDiv">
-                                <h2>${musicLibrary.songs[i]}</h2>
+
+                                <h2>${library.music[i].Song}</h2>
                                 <ul class="musicInfo">
-                                  <li>${musicLibrary.artist[i]}</li>
-                                  <li>${musicLibrary.album[i]}</li>
-                                  <li class="no-borderR">${musicLibrary.genre[i]}</li>
+                                  <li>${library.music[i].Artist}</li>
+                                  <li>${library.music[i].Album}</li>
+                                  <li class="no-borderR">${library.music[i].Genre}</li>
                                 </ul>
-                              </div>
+                                <button id="delButton" class="floatR">Delete</button>
+
                            `
 mainDiv.appendChild(songDiv)
 
+if (i === library.music.length - 1) {
+  var moreDiv = document.createElement("div")
+  moreDiv.classList.add("more-div")
+  moreDiv.innerHTML = `<button id="more-btn">More</button>`
+  mainDiv.appendChild(moreDiv)
+  buttonListeners()
+}
+
+function buttonListeners() {
+document.querySelector(".mainDiv").addEventListener("click", (e) => {
+  if (e.target.textContent === "Delete") {
+    console.log(e);
+    var parent = e.target.parentNode;
+    mainDiv.removeChild(parent)
+
+  } else if (e.target.textContent === "More"){
+
+  var musicRequest = new XMLHttpRequest();
+  musicRequest.addEventListener("load", (e) => {
+    data2 = JSON.parse(e.target.responseText)
+    console.log(data2);
+    addUpdatedMusic(data2)
+    mainDiv.removeChild(document.querySelector(".more-div"))
+
+  })
+  musicRequest.open("GET", "musicLibrary2.json")
+  musicRequest.send()
+
+
+
+}
+})
+}
+
+
+
   }
 }
+
+
 function showViewMusicView() {
   addMusicView.classList.toggle("hidden")
   formDiv.classList.toggle("hidden")
@@ -120,19 +91,29 @@ navBar.addEventListener("click", (e) => {
   }
 })
 
-addMusicView.addEventListener("click", (e) => {
-  if (e.target.tagName === "BUTTON") {
-    musicLibrary.songs.push(addMusicView.querySelector("#add-music-song-input").value)
-    musicLibrary.artist.push(addMusicView.querySelector("#add-music-artist-input").value)
-    musicLibrary.album.push(addMusicView.querySelector("#add-music-album-input").value)
-    musicLibrary.genre.push(addMusicView.querySelector("#add-music-genre-input").value)
-    console.log(musicLibrary);
-    addUpdatedMusic()
-    showViewMusicView()
-    clearInputs()
-  }
+
+
+
+
+// addMusicView.addEventListener("click", (e) => {
+//   if (e.target.tagName === "BUTTON") {
+//     var num = library.music.length;
+//     console.log(num);
+//     library.music[num].Song = addMusicView.querySelector("#add-music-song-input").value
+//     library.music[num].Artist = addMusicView.querySelector("#add-music-artist-input").value
+//     library.music[num].Album = addMusicView.querySelector("#add-music-album-input").value
+//     library.music[num].Genre = addMusicView.querySelector("#add-music-genre-input").value
+//     addUpdatedMusic()
+//     showViewMusicView()
+//     clearInputs()
+//   }
+// })
+
+var musicRequest = new XMLHttpRequest();
+musicRequest.addEventListener("load", (e) => {
+  var data1 = JSON.parse(e.target.responseText)
+  // console.log(library);
+  addUpdatedMusic(data1)
 })
-
-
-
-addUpdatedMusic()
+musicRequest.open("GET", "musicLibrary.json")
+musicRequest.send()
